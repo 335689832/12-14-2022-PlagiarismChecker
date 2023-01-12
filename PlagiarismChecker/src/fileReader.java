bd66a9429007c6cf95fe0ab0d8850f462ae71508
Zara <335707584@gapps.yrdsb.ca>
2023-01-12 14:27:33 -0500

    filereader (needs to be tested)

---
+++ PlagiarismChecker/src/fileReader.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.List;
+
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
+public class fileReader {
+  
+  public static void readDocxFile(String fileName) {
+
+		try {
+			File file = new File(fileName);
+			FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+
+			XWPFDocument document = new XWPFDocument(fis);
+
+			List<XWPFParagraph> paragraphs = document.getParagraphs();
+			
+			System.out.println("Total no of paragraph "+ paragraphs.size());
+			for (XWPFParagraph para : paragraphs) {
+				System.out.println(para.getText());
+			}
+			fis.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+}
+    public static void main(String[] args) {
+
+		readDocxFile("C:\\Test.docx");
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
